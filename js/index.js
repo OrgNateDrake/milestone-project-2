3ee13cea9e4a7c630bc153610e69bf8decaf57b7
Nathan Drake <133364751+OrgNateDrake@users.noreply.github.com>
2023-08-13 11:50:35 +0100

    Feat:Add theme pages and JS

--- js/index.js
+++ js/index.js
@@ -41,3 +41,28 @@
 			cells.push(obj[key]);
 		}
 	}
+    
+    var CopyMatrix = function(M) {
+		return M.map(function(row) {
+			return row.slice();
+		});
+	};
+	var sumGF2 = function(a, b) {
+		return (a !== b) ? one : zero;
+	};
+	var multGF2 = function(a, b) {
+		return a * b;
+	};
+	var divGF2 = function(a, b) {
+		if (b === zero) {
+			console.log('Trying to divide by zero!');
+		}
+		return a;
+	};
+	var dotProduct = function(v1, v2) {
+		return v1.map(function(e, index) {
+			return multGF2(e, v2[index]);
+		}).reduce(function(a, b) {
+			return sumGF2(a, b);
+		}, 0);
+	};
